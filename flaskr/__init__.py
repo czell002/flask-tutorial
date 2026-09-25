0b63ced57895ec0941bb325e2ffb0f134cb0aca1
czell002 <czell002@odu.edu>
2026-09-25 13:30:06 -0400

    Completed Define and Access the Database

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,6 +6,7 @@ from flask import Flask
 def create_app(test_config=None):
 	# create and configure the Flask instance (the app)
 	app = Flask(__name__, instance_relative_config=True)
+	
 	app.config.from_mapping(
 		SECRET_KEY='dev',
 		DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -25,5 +26,8 @@ def create_app(test_config=None):
 	@app.route('/hello')
 	def hello():
 		return 'Hello, World!'
+	
+	from . import db
+	db.init_app(app)
 	
 	return app
